86796f905d3aff9c0534525935f7779ef95aa8b1
RubyChilds <rchilds@theladders.com>
2014-02-02 19:12:12 -0500

    completed q3

--- week 3/socialNetworkModificationCore.sql
+++ week 3/socialNetworkModificationCore.sql
@@ -18,3 +18,10 @@ SELECT L.ID1 FROM LIKES L WHERE Likes.ID1 = L.ID2
 
 
 -- 3. For all cases where A is friends with B, and B is friends with C, add a new friendship for the pair A and C. Do not add duplicate friendships, friendships that already exist, or friendships with oneself.
+
+
+INSERT INTO Friend
+SELECT DISTINCT F1.ID1, F2.ID2
+FROM Friend F1, Friend F2
+WEHER F1.ID2 = F2.ID1 AND F1.ID1<>F2.ID2 
+      and F1.ID1 NOT IN (SELECT F3.ID1 FROM Friend F3 WHERE F3.ID2=F2.ID2)
